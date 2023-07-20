12d5f1080ffc52e0543f5fd9c60fe2fdf2dd8d03
Manjot Bal <manjot@manjotbal.ca>
2023-07-19 22:12:20 -0600

    Generate a simple Gaussian filter given a standard deviation and size.

--- src/ImageFilter_internal.hpp
+++ src/ImageFilter_internal.hpp
@@ -18,4 +18,6 @@ std::vector<uchar> computeBoxBlur(
 		dim3 gridDim);
 
 std::vector<uchar> padRepeatEdge(std::shared_ptr<const Image> image, uint2 filterDim, uint& width, uint& height);
+
+std::vector<float> generateGaussianFilter(float sigma, uint dim);
 }} // namespace ImageFilter::internal
